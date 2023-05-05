b196d11bed7312f2a755bdd1a13349030865647d
Obizzi <kramzyvine@icloud.com>
2023-05-05 21:59:43 +0300

    Created the basics for Part 1

--- X_and_Oz.py
+++ X_and_Oz.py
@@ -1,12 +1,14 @@
 # Part 1: The Display
+Xs = ["X", "X", "X"]
+Os = ["O", "O", "O"]
+display_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+reverse_display_list = sorted(display_list, reverse=True)
+a = True
+
+
 def part_1():
-    Xs = ["X", "X", "X"]
-    Os = ["O", "O", "O"]
-    display_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    reverse_display_list = sorted(display_list, reverse=True)
     for i in range(0, len(display_list), 3):
         print(display_list[i:i + 3])
-
     # the verticals
     if display_list[0::3] or reverse_display_list[0::3] == Xs:
         print("The Xs win it!!")
@@ -47,4 +49,20 @@ def part_1():
     else:
         print("It's a draw!!")
 
+part_1()
 
+for i in range(9):
+    if a:
+        selector = int(input("Enter number: "))
+        for item in display_list:
+            if item == selector:
+                item = "X"
+        a = False
+        part_1()
+    else:
+        selector = int(input("Enter number: "))
+        for item in display_list:
+            if item == selector:
+                item = "O"
+        a = True
+        part_1()
--- tests.py
+++ tests.py
@@ -1,2 +1,3 @@
-display_list = [4, 5, 6, 7, 8, 9]
-print(sorted)
+a = [1, 2, 3]
+b = [4, 5, 6]
+print(*zip(a, b))
